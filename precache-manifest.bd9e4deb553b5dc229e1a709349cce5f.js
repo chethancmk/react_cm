796a210397af066deb0accb037e00a30577ba2fe
Name <you@example.com>
2019-11-17 13:25:14 +0530

    Updates

---
+++ precache-manifest.bd9e4deb553b5dc229e1a709349cce5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e033a52509e273917615bb8cdd0e7499",
+    "url": "/cm_react/index.html"
+  },
+  {
+    "revision": "d9d0e28da093d4ee405a",
+    "url": "/cm_react/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "718424dae707a5412ea4",
+    "url": "/cm_react/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d9d0e28da093d4ee405a",
+    "url": "/cm_react/static/js/2.73086b53.chunk.js"
+  },
+  {
+    "revision": "718424dae707a5412ea4",
+    "url": "/cm_react/static/js/main.97fb2287.chunk.js"
+  },
+  {
+    "revision": "c1e1949fd0b3fbadffb0",
+    "url": "/cm_react/static/js/runtime-main.bb61fd1a.js"
+  }
+]);
